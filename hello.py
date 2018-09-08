b4ef54e8f21fdabc4f7bfb1c3bd837a545d0b855
leon5842 <leon58.csie@gmail.com>
2018-09-08 05:17:21 -0700

    python: test commit
    upload from ubuntu
    
    signed-off-by: leon5842 <leon58.csie@gmail.com>

--- hello.py
+++ hello.py
@@ -6,3 +6,10 @@ x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the r
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
 print("wa hahaha")
+
+
+
+
+
+print ("")
+
