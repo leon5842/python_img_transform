aa257eb1916aee1b7d64104ef8d0892b70feb751
leon5842 <43083119+leon5842@users.noreply.github.com>
2019-03-24 01:19:39 +0800

    plot update
    update new settings

--- hello.py
+++ hello.py
@@ -1,8 +1,23 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.pylab as pylab
 import numpy as np
+
+plt.style.use("ggplot")
+params = {
+            'axes.labelsize': '16',
+            'xtick.labelsize': '16',
+            'ytick.labelsize': '13',
+            'lines.linewidth': '10',
+            'legend.fontsize': '20',
+            'figure.figsize': '100, 24'  # set figure size
+        }
+pylab.rcParams.update(params)
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
-print("wa hahaha")
+plt.savefig("d:/filename.png", dpi=300) 
+
+
+
+
