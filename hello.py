8bfc2df757c199f018634cbd7c2e7a0a985be0d9
leon5842 <43083119+leon5842@users.noreply.github.com>
2019-03-24 10:02:54 +0800

    style update
    as title

--- hello.py
+++ hello.py
@@ -3,20 +3,23 @@ import matplotlib as mpl
 import matplotlib.pylab as pylab
 import numpy as np
 
-plt.style.use("ggplot")
+plt.style.use("seaborn")
 params = {
             'axes.labelsize': '16',
-            'xtick.labelsize': '16',
-            'ytick.labelsize': '13',
+            'xtick.labelsize': '30',
+            'ytick.labelsize': '30',
             'lines.linewidth': '10',
-            'legend.fontsize': '20',
-            'figure.figsize': '100, 24'  # set figure size
+            'legend.fontsize': '30',
+            'font.size': '20',
+            'xtick.major.size'    : 4        ,
+            'figure.figsize': '28, 24'  # set figure size
         }
 pylab.rcParams.update(params)
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.savefig("d:/filename.png", dpi=300) 
+plt.xlabel("ff", fontsize=100)
+plt.savefig("d:/filename.png", dpi=90) 
 
 
 
