2532a30f164448cd65e53a0d9a12bfac1d4abe32
leon5842 <43083119+leon5842@users.noreply.github.com>
2019-03-24 22:29:11 +0800

    pdf update
    as title

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.pylab as pylab
 import numpy as np
+import csv
+from matplotlib.backends.backend_pdf import PdfPages
 
 plt.style.use("seaborn")
 params = {
@@ -10,16 +12,31 @@ params = {
             'ytick.labelsize': '30',
             'lines.linewidth': '10',
             'legend.fontsize': '30',
-            'font.size': '20',
+            'font.size': '10',
             'xtick.major.size'    : 4        ,
-            'figure.figsize': '28, 24'  # set figure size
+            'figure.figsize': '30, 24'  # set figure size
         }
+d = [12,32,45,33,99,12]
+o = [0,1,2,3,4,5,6]
 pylab.rcParams.update(params)
+zip(d)
+with open('person.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(d)
+csvFile.close()
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.xlabel("ff", fontsize=100)
-plt.savefig("d:/filename.png", dpi=90) 
+# Create the PdfPages object to which we will save the pages:
+# The with statement makes sure that the PdfPages object is closed properly at
+# the end of the block, even if an Exception occurs.
+with PdfPages('multipage_pdf.pdf') as pdf:
+
+    x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+    plt.plot(x, np.sin(x))       # Plot the sine of each x point
+    plt.title('Page One')
+    plt.xlabel("ff", fontsize=100)
+    pdf.savefig()
+    plt.close()
+
 
 
 
---
+++ pdf.py
@@ -0,0 +1,15 @@
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Create the PdfPages object to which we will save the pages:
+# The with statement makes sure that the PdfPages object is closed properly at
+# the end of the block, even if an Exception occurs.
+with PdfPages('multipage_pdf.pdf') as pdf:
+    plt.figure(figsize=(3, 3))
+    plt.plot(range(7), [3, 1, 4, 1, 5, 9, 2], 'r-o')
+    plt.title('Page One')
+    pdf.savefig()  # saves the current figure into a pdf page
+    plt.close()
+
